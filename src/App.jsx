6f2b67737a77bc881eafce0f82493bf2ae12a509
rishavtiwar22 <rishav@navgurukul.org>
2025-11-18 11:39:38 +0530

    Starter template

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <>
+      <div className='bg-gray-300'>
+        <p>Hello India!!!</p>
+      </div>
+        
+    </>
+  )
+}
+
+export default App
